6d64b1f0cb214816267f9230def339d5732c9115
Ghofrane Rahmouni <92804111+ghofrane0155@users.noreply.github.com>
2024-07-13 14:32:00 +0100

    working on jwtService

---
+++ back/stock/src/main/java/tn/esprit/stock/security/JwtService.java
@@ -0,0 +1,9 @@
+package tn.esprit.stock.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String jwt) {
+    }
+}
---
+++ back/stock/src/main/java/tn/esprit/stock/security/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package tn.esprit.stock.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import tn.esprit.stock.user.IUserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final IUserRepository repository;
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+            return repository.findByEmail(userEmail)
+                    .orElseThrow(()->new UsernameNotFoundException("User not found"));
+    }
+}
