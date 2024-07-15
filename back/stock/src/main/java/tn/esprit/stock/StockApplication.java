a1a1d15a7234f5699eefa838a275cc7814d9b977
Ghofrane Rahmouni <92804111+ghofrane0155@users.noreply.github.com>
2024-07-15 14:05:40 +0100

    test registration done but did not recieve mail

--- back/stock/src/main/java/tn/esprit/stock/StockApplication.java
+++ back/stock/src/main/java/tn/esprit/stock/StockApplication.java
@@ -1,15 +1,29 @@
 package tn.esprit.stock;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import tn.esprit.stock.role.IRoleRepository;
+import tn.esprit.stock.role.Role;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class StockApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(IRoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
+	}
 }
