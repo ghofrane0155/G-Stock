24c2ab065210027dafb798be4864414870a51b94
Ghofrane Rahmouni <92804111+ghofrane0155@users.noreply.github.com>
2024-07-23 12:23:04 +0100

    account settings & view profile integrated

--- front/stock-front/src/app/components/account-settings/account-settings.component.ts
+++ front/stock-front/src/app/components/account-settings/account-settings.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-account-settings',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './account-settings.component.html',
   styleUrl: './account-settings.component.scss'
 })
 export class AccountSettingsComponent {
+  password = {
+    oldPassword: '',
+    newPassword: '',
+    confirmNewPassword: ''
+  };
 
+  resetPassword(): void {
+    if (this.password.newPassword === this.password.confirmNewPassword) {
+      // Logic to reset password
+      console.log('Resetting password:', this.password);
+    } else {
+      console.error('New passwords do not match.');
+    }
+  }
 }
