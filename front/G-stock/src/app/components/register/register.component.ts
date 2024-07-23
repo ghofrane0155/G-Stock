0187618bc05b94df59de5d413cbe8a55ea934618
Ghofrane Rahmouni <92804111+ghofrane0155@users.noreply.github.com>
2024-07-23 16:49:17 +0100

    register template integrated

--- front/G-stock/src/app/components/register/register.component.ts
+++ front/G-stock/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { RegistrationRequest } from '../../services/models';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../services/services';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  authRequest: RegistrationRequest = { firstname: '', lastname: '', email: '', password: '' };
+  errorMsg: Array<string> = [];
 
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {}
+
+  register() {
+    this.errorMsg = [];
+   
+  }
+
+  login() {
+    this.router.navigate(['login']);
+  }
 }
