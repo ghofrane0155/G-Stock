440d9a78eda3eb2527d6e6379746697c5fe50cb8
Ghofrane Rahmouni <92804111+ghofrane0155@users.noreply.github.com>
2024-07-23 10:53:41 +0100

    footer integrated

--- front/stock-front/src/app/app.component.ts
+++ front/stock-front/src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { routes } from './app.routes';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, NavbarComponent],
+  imports: [RouterOutlet, RouterModule, SidebarComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'stock-front';
+
 }
--- front/stock-front/src/app/app.routes.ts
+++ front/stock-front/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import {  Routes } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ViewProfileComponent } from './components/view-profile/view-profile.component';
+import { AccountSettingsComponent } from './components/account-settings/account-settings.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // Default route
-    { path: 'base', component: SidebarComponent },
-    { path: 'sidebar', component: SidebarComponent }
+    { path: 'sidebar', component: SidebarComponent },
+    { path: 'view-profile', component: ViewProfileComponent },
+    { path: 'account-settings', component: AccountSettingsComponent },
+    // { path: '**', redirectTo: '/dashboard' } ,
 ];
