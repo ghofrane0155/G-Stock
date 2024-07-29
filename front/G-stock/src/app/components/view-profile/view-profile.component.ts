9599713f73a849dc0137161231885b0d2f8aa5e7
Ghofrane Rahmouni <92804111+ghofrane0155@users.noreply.github.com>
2024-07-29 10:52:02 +0100

    works

--- front/G-stock/src/app/components/view-profile/view-profile.component.ts
+++ front/G-stock/src/app/components/view-profile/view-profile.component.ts
@@ -1,46 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { UserControllerService } from '../../services/services';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-view-profile',
   templateUrl: './view-profile.component.html',
   styleUrl: './view-profile.component.scss'
 })
-export class ViewProfileComponent implements OnInit {
-  isEditing = false;  
-  currentUser: any = {}; 
-
-  constructor() { }
-
-  ngOnInit(): void { 
-    const userJson = localStorage.getItem('user');
-    if (userJson) {
-      try {
-        this.currentUser = JSON.parse(userJson);
-        console.log('currentUser:', this.currentUser);
-      } catch (error) {
-        console.error('Failed to parse user data from local storage:', error);
-      }
-    } else {
-      console.error('No user data found in local storage');
-    }
-  }
-
-  toggleEdit(): void {
-    if (this.isEditing) {
-      // Logic to save changes
-      console.log('Saving changes:', this.currentUser);
-    }
-    this.isEditing = !this.isEditing;
-  }
-
-  cancelEdit(): void {
-    // Logic to handle canceling edits (e.g., reset form to original values)
-    console.log('Edit canceled');
-    this.isEditing = false;
-  }
-
-  changePhoto(): void {
-    // Logic to handle photo change
-    alert('Change Photo button clicked');
-  }
+export class ViewProfileComponent {
+  
 }
