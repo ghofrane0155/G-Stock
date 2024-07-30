152fff0a82485b168cda4b43fe5c8b1a00607600
Ghofrane Rahmouni <92804111+ghofrane0155@users.noreply.github.com>
2024-07-30 14:34:40 +0100

    correct some errors

--- front/G-stock/src/app/components/navbar/navbar.component.ts
+++ front/G-stock/src/app/components/navbar/navbar.component.ts
@@ -1,25 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
   currentUser: any = {};
+
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
 
   ngOnInit() {
     this.loadCurrentUser();
   }
 
   loadCurrentUser() {
-    const userData = localStorage.getItem('user');
-    if (userData) {
-      this.currentUser = JSON.parse(userData);
+    if (isPlatformBrowser(this.platformId)) {
+      const userData = localStorage.getItem('user');
+      if (userData) {
+        this.currentUser = JSON.parse(userData);
+      }
     }
   }
 
-  logout(){
-    localStorage.removeItem('user');
+  logout() {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem('user');
+      // Add additional logout logic here if needed
+    }
   }
 }
